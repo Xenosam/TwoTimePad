dc80f846f746d554d3927efe14ee8131fab57112
Andrew McHardy <zcva984@live.rhul.ac.uk>
2018-03-07 15:49:09 +0000

    Smoothing and better Model tests
    Seperated required files into a resource folder, created new tests for
    laplace smoothing and a seperate model based on the AnalysisPair class
    which binds a character sequence and a probability.

--- src/tests/TestFile.java
+++ src/tests/TestFile.java
@@ -2,38 +2,114 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import com.aliasi.lm.NGramProcessLM;
 
+import project.AnalysisPair;
 import project.LanguageModel;
 
 public class TestFile {
 
-	LanguageModel lm = new LanguageModel();
-	NGramProcessLM model = new NGramProcessLM(7);
-	
+	NGramProcessLM model, laplace;
+
+	/**
+	 * Setup method for initialising test variables
+	 * 
+	 * @throws Exception
+	 *             Runtime Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
-		
+		model = LanguageModel.train(LanguageModel.createModel(3),
+				"C:/Users/Andrew/workspace/TwoTimeNLM/resources/corpus/A Tale of Two Cities - Charles Dickens.txt");
+		laplace = LanguageModel.smoothingLaplace(model);
 	}
-	
-	/*
+
+	/**
 	 * Creates Models and tests that NGram length is correct
 	 */
 	@Test
 	public void testOne() {
-		//assertEquals("String", $ExpectedOutcome, methodForTest());
-	    assertEquals("TEST1: Create Model", 7, LanguageModel.createModel(7).maxNGram());
+		// assertEquals("String", $ExpectedOutcome, methodForTest());
+		assertEquals("TEST1: Create Model", 3, model.maxNGram());
 	}
-	
-	/*
-	 * Recieves useful information from model
+
+	/**
+	 * Recieves useful information from model and grabs the probability for a
+	 * given n-gram
 	 */
 	@Test
-	public void testTwo() {
-		
-		assertEquals("TEST1: Create Model", 3, (int)LanguageModel.createModel(7).log2ConditionalEstimate("and i a"));
+	public void testTwo() throws IOException {
+		assertEquals("TEST2: Access Model", 7, (int) Integer.valueOf((int) (model.prob("and") * 1000)));
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testThree() {
+		AnalysisPair aP = new AnalysisPair("and", 0.007321452);
+		assertEquals("TEST3: Analysis Pair", "and, 0.007321452", aP.toString());
+	}
+
+	/**
+	 * Grab the most likely N-Grams given an N-1Gram
+	 */
+	@Test
+	public void testFour() {
+		AnalysisPair aP = new AnalysisPair("and", 0.007442417374451658);
+		assertEquals("TEST4: Testing most likely NGrams", aP.toString(),
+				LanguageModel.likelyNGrams("an", model, 1)[0].toString());
+	}
+
+	/**
+	 * Test Save/Load functionality of the program
+	 */
+	@Test
+	public void testFive() {
+		LanguageModel.saveToFile("testfile", model);
+		NGramProcessLM model2 = LanguageModel.loadFromFile("testfile");
+		assertEquals("TEST5: Saving/Loading Model to/from File", (int) (model2.prob("and") * 1000),
+				(int) (model.prob("and") * 1000));
+	}
+
+	/**
+	 * Creating a seperate model using the AnalysisPair class for more helpful
+	 * analysis
+	 */
+	@Test
+	public void testSix() {
+		AnalysisPair[] aP = LanguageModel.createAPModel(model, 3);
+		int correctindex = 0;
+		for (int i = 0; i < aP.length; i++) {
+			if (aP[i].getNgram().equals("and")) {
+				correctindex = i;
+				break;
+			}
+		}
+
+		assertEquals("TEST6: AnalysisPair model", (int) (model.prob("and") * 1000),
+				(int) (aP[correctindex].getProbability() * 1000));
+	}
+
+	/**
+	 * Language Smoothing Test (Laplace)
+	 */
+	@Test
+	public void testSeven() {
+		assertEquals("TEST7: Laplace Smoothing", (int) (0 * 1000),
+				(int) LanguageModel.smoothingLaplace(model).prob("and") * 1000);
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	public void test() {
+		assertEquals("TEST:", 0, 0);
 	}
 }
